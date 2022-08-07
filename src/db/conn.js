865a3ee131c6b4715cd6d6b51665bb32b1aeceb8
pankaj7073 <pankajvaishnav128@gmail.com>
2022-08-07 16:02:06 +0530

    my portfolio

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    console.log('Database connected successfully')
+})
+.catch((error)=>{
+    console.log('Database not connected');
+    console.log(error);
+});
